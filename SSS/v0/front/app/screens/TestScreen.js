51ff4a43ef1644c46fd848f3540a5437b9b21e7a
Julie <jihyeo2@illinois.edu>
2020-08-18 23:07:20 +0900

    added menus in StoresMenuAddScreen

--- SSS/v0/front/app/screens/TestScreen.js
+++ SSS/v0/front/app/screens/TestScreen.js
@@ -1,24 +1,46 @@
-import React, { useEffect } from "react";
-import { Text, FlatList } from "react-native";
-import catApi from "../api/categories";
-import useApi from "../hooks/useApi";
+import React from "react";
+import { StyleSheet, ScrollView } from "react-native";
+import * as Yup from "yup";
+
+import Screen from "../components/Screen";
+import { AppForm as Form, SubmitButton } from "../components/forms";
+import AppText from "../components/AppText";
+import FormMenuPicker from "../components/forms/FormMenuPicker";
 
 function TestScreen(props) {
-  const getListingsApi = useApi(catApi.getCategories);
-
-  useEffect(() => {
-    getListingsApi.request();
-  }, []);
+  const validationSchema = Yup.object().shape({
+    menus: Yup.array().min(1, "Please select at least one image."),
+  });
 
   return (
-    <FlatList
-      data={getListingsApi.data}
-      keyExtractor={(item) => item._id.toString()}
-      numColumns={2}
-      width="100%"
-      renderItem={({ item }) => <Text>{item.label}</Text>}
-    />
+    <Screen style={styles.container}>
+      <ScrollView>
+        <AppText>Menu/Service</AppText>
+        <Form
+          initialValues={{
+            menus: [],
+          }}
+          onSubmit={(listing) =>
+            console.log("finally menu items are submitted!", listing)
+          }
+          validationSchema={validationSchema}
+        >
+          <FormMenuPicker name="menus" />
+          <SubmitButton title="Submit" />
+        </Form>
+      </ScrollView>
+    </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    paddingTop: 20,
+  },
+  images: {
+    flexDirection: "row",
+  },
+});
 
 export default TestScreen;
